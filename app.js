a77f14bbad9d4355e531c2a735c18a2ac160b189
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-02-02 20:28:13 +0530

    app.js

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ var express = require("express"),
     bodyParser = require('body-parser'),
     mysql = require('mysql');
 
-  
-app.use(bodyParser.json()); 
+
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
-})); 
+}));
 app.set('port', (process.env.PORT || 8000));
 app.use(express.static(__dirname + '/public'));
 
@@ -27,195 +27,196 @@ app.listen(app.get('port'), function () {
 
 var con = mysql.createConnection({
     host: "localhost",
-    user : 'root',
-    password : '',
-    database : 'hallbooking',
-    multipleStatements: true});
-
-
-con.connect(function(error){
-if(error){
-console.log("Error Connecting Yo!");
-return;
-}else{
-    console.log(" Connected Yo!");
-}
+    user: 'root',
+    password: '',
+    database: 'hallbooking',
+    multipleStatements: true
 });
 
-app.get('/',function(req,res){
+
+con.connect(function (error) {
+    if (error) {
+        console.log("Error Connecting Yo!");
+        return;
+    } else {
+        console.log(" Connected Yo!");
+    }
+});
+
+app.get('/', function (req, res) {
     res.render('index');
 })
-app.get('/index',function(req,res){
+app.get('/index', function (req, res) {
     res.render('index');
 })
 
-app.get('/login',function(req,res){
+app.get('/login', function (req, res) {
     res.render('selectUser');
 })
 
-app.get('/deanLogin',function(req,res){
-    
+app.get('/deanLogin', function (req, res) {
+
     res.render('deanLogin');
 })
 
-app.get('/facilityLogin',function(req,res){
+app.get('/facilityLogin', function (req, res) {
     res.render('facilityLogin');
 })
 
-app.get('/facultyLogin',function(req,res){
+app.get('/facultyLogin', function (req, res) {
     res.render('facultyLogin');
 })
 
-app.get('/clubLogin',function(req,res){
+app.get('/clubLogin', function (req, res) {
     res.render('clubLogin');
 })
 
-app.get('/request',function(req,res){
+app.get('/request', function (req, res) {
     res.render('request');
 })
 
 
 
 
-app.post('/dashboardDean',function(req,res){
+app.post('/dashboardDean', function (req, res) {
     var username = req.body.username;
     var password = req.body.password;
 
-    con.query("select * from users where Username='"+username+"' and Designation='Dean';", function(err,rows){
-        if(!err){
-            if(rows.length>0){
+    con.query("select * from users where Username='" + username + "' and Designation='Dean';", function (err, rows) {
+        if (!err) {
+            if (rows.length > 0) {
 
-                 if(password===rows[0].Password){
+                if (password === rows[0].Password) {
                     res.render("dashboardDean");
-                }else{
+                } else {
                     console.log("Password is Wrong Buddy!");
-                    res.render("error",{message:"Password is Wrong Buddy!"});
-                }    
-                
-            }else{
+                    res.render("error", { message: "Password is Wrong Buddy!" });
+                }
+
+            } else {
                 console.log("Username Not Found!");
-                res.render("error",{message:"Username Not Found!"});
+                res.render("error", { message: "Username Not Found!" });
             }
-            
-                
-        }else{
-            
-            res.render("error",{message:"Dont Poke Your Nose where you don't Belong!"});
+
+
+        } else {
+
+            res.render("error", { message: "Dont Poke Your Nose where you don't Belong!" });
         }
     });
-    
-  
+
+
     //res.render('dashboardDean');
 })
 
-app.post('/dashboardFaculty',function(req,res){
+app.post('/dashboardFaculty', function (req, res) {
     var username = req.body.username;
     var password = req.body.password;
 
-    con.query("select * from users where Username='"+username+"' and Designation='Faculty';", function(err,rows){
-        if(!err){
-            if(rows.length>0){
+    con.query("select * from users where Username='" + username + "' and Designation='Faculty';", function (err, rows) {
+        if (!err) {
+            if (rows.length > 0) {
 
-                 if(password===rows[0].Password){
+                if (password === rows[0].Password) {
                     res.render("dashboardFaculty");
-                }else{
+                } else {
                     console.log("Password is Wrong Buddy!");
-                    res.render("error",{message:"Password is Wrong Buddy!"});
-                }    
-                
-            }else{
+                    res.render("error", { message: "Password is Wrong Buddy!" });
+                }
+
+            } else {
                 console.log("Username Not Found!");
-                res.render("error",{message:"Username Not Found!"});
+                res.render("error", { message: "Username Not Found!" });
             }
-            
-                
-        }else{
+
+
+        } else {
             console.log("Dont Poke Your Nose where you don't Belong!");
-            res.render("error",{message:"Dont Poke Your Nose where you don't Belong!"});
+            res.render("error", { message: "Dont Poke Your Nose where you don't Belong!" });
         }
     });
-    
-  
+
+
 })
 
-app.post('/dashboardFacility',function(req,res){
+app.post('/dashboardFacility', function (req, res) {
     var username = req.body.username;
     var password = req.body.password;
 
-    con.query("select * from users where Username='"+username+"' and Designation='Facility';", function(err,rows){
-        if(!err){
-            if(rows.length>0){
+    con.query("select * from users where Username='" + username + "' and Designation='Facility';", function (err, rows) {
+        if (!err) {
+            if (rows.length > 0) {
 
-                 if(password===rows[0].Password){
+                if (password === rows[0].Password) {
                     res.render("dashboardFacility");
-                }else{
+                } else {
                     console.log("Password is Wrong Buddy!");
-                    res.render("error",{message:"Password is Wrong Buddy!"});
-                }    
-                
-            }else{
+                    res.render("error", { message: "Password is Wrong Buddy!" });
+                }
+
+            } else {
                 console.log("Username Not Found!");
-                res.render("error",{message:"Username Not Found!"});
+                res.render("error", { message: "Username Not Found!" });
             }
-            
-                
-        }else{
+
+
+        } else {
             console.log("Dont Poke Your Nose where you don't Belong!");
-            res.render("error",{message:"Dont Poke Your Nose where you don't Belong!"});
+            res.render("error", { message: "Dont Poke Your Nose where you don't Belong!" });
         }
     });
-    
-  
+
+
     //res.render('dashboardFacility');
 })
 
-app.post('/dashboardClub',function(req,res){
+app.post('/dashboardClub', function (req, res) {
     var username = req.body.username;
     var password = req.body.password;
 
-    con.query("select * from users where Username='"+username+"' and Designation='Dhara';", function(err,rows){
-        if(!err){
-            if(rows.length>0){
+    con.query("select * from users where Username='" + username + "' and Designation='Dhara';", function (err, rows) {
+        if (!err) {
+            if (rows.length > 0) {
 
-                 if(password===rows[0].Password){
+                if (password === rows[0].Password) {
                     res.render("dashboardClub");
-                }else{
+                } else {
                     console.log("Password is Wrong Buddy!");
-                    res.render("error",{message:"Password is Wrong Buddy!"});
-                }    
-                
-            }else{
+                    res.render("error", { message: "Password is Wrong Buddy!" });
+                }
+
+            } else {
                 console.log("Username Not Found!");
-                res.render("error",{message:"Username Not Found!"});
+                res.render("error", { message: "Username Not Found!" });
             }
-            
-                
-        }else{
+
+
+        } else {
             console.log("Dont Poke Your Nose where you don't Belong!");
-            res.render("error",{message:"Dont Poke Your Nose where you don't Belong!"});
+            res.render("error", { message: "Dont Poke Your Nose where you don't Belong!" });
         }
     });
-    
-  
+
+
     //res.render('dashboardDhara');
 })
 
-app.get('/dashboardFaculty',function(req,res){
+app.get('/dashboardFaculty', function (req, res) {
     res.render('dashboardFaculty');
 })
-app.get('/dashboardFacility',function(req,res){
+app.get('/dashboardFacility', function (req, res) {
     res.render('dashboardFacility');
 })
-app.get('/dashboardClub',function(req,res){
+app.get('/dashboardClub', function (req, res) {
     res.render('dashboardClub');
 })
-app.get('/anagha',function(req,res){
+app.get('/anagha', function (req, res) {
     res.render('anagha');
 })
 
 
 
-app.post('/makeRequest',function(req,res){
+app.post('/makeRequest', function (req, res) {
     var fname = req.body.first_name;
     var lname = req.body.last_name;
     var mname = req.body.mentor_name;
@@ -227,32 +228,33 @@ app.post('/makeRequest',function(req,res){
     var startTime = req.body.from_slot;
     var stopTime = req.body.to_slot;
     var edate = req.body.date_slot;
-        
+
     var re = {
-        Fname:fname,
-        Lname:lname,
-        ClubName:cname,
-        FacultyOrMentorName:mname,
+        Fname: fname,
+        Lname: lname,
+        ClubName: cname,
+        FacultyOrMentorName: mname,
         ClubSecName: sname,
-        Wing:wname,
-        Hname:hname,
-        EventDate:edate,
-        FromTime:startTime,
-        ToTime:stopTime,
-        EventName:ename
+        Wing: wname,
+        Hname: hname,
+        EventDate: edate,
+        FromTime: startTime,
+        ToTime: stopTime,
+        EventName: ename
 
     };
 
 
-    con.query('insert into requests set ?',re,function(err,result){
-        if(!err){console.log("success");
-                res.render("dashboardClub",{message:"Yaay! It Worked!!!"});
-                }else{
-                    console.log(err);
-                    res.render("error",{message:"Not working."});
-              }
-           });
-        });
+    con.query('insert into requests set ?', re, function (err, result) {
+        if (!err) {
+            console.log("success");
+            res.render("dashboardClub", { message: "Yaay! It Worked!!!" });
+        } else {
+            console.log(err);
+            res.render("error", { message: "Not working." });
+        }
+    });
+});
 <<<<<<< HEAD
 
 
@@ -264,50 +266,50 @@ app.post('/makeRequest',function(req,res){
 
 
 >>>>>>> e30c22b20fcc39f9701e29e70eb563a377d34a3b
-app.get('/pdean',function(req,res){
+app.get('/pdean', function (req, res) {
     var request = "Select * from requests;"
 
-    con.query(request,function(req,data){
+    con.query(request, function (req, data) {
         console.log(data);
-        res.render('pdean',{res:data});
+        res.render('pdean', { res: data });
     });
 })
 
-app.get('/pfacility',function(req,res){
+app.get('/pfacility', function (req, res) {
     var request = "Select * from requests;"
 
-    con.query(request,function(req,data){
+    con.query(request, function (req, data) {
         console.log(data);
-        res.render('pfacility',{res:data});
+        res.render('pfacility', { res: data });
     });
 })
 
 <<<<<<< HEAD
 
 
-app.get('/grantPermission',function(req,res){
+app.get('/grantPermission', function (req, res) {
     rid = req.query.rid;
     status = JSON.stringify(req.query.status);
-    console.log(status+"   "+ rid)
-    var query = "update requests set pd="+status+" where rid="+ rid+" ;"
+    console.log(status + "   " + rid)
+    var query = "update requests set pd=" + status + " where rid=" + rid + " ;"
     console.log(query);
     query1 = "select * from requests;"
     console.log(query1);
-    
-        con.query(query,function(err,result){
-            console.log("ash")
-             res.render("dashboardDean",{message:"Cool"});
-                   });
-                   
-        });
 
-        
+    con.query(query, function (err, result) {
+        console.log("ash")
+        res.render("dashboardDean", { message: "Cool" });
+    });
+
+});
+
+
 =======
 /*app.get('/grantPermission',function(req,res){
 
      var len= req.query.len;
      console.log("the length"+len);
-    
+
      for(var param in req.query){
         var bc=req.query[param];
          if(bc.length!=0)
@@ -321,120 +323,121 @@ app.get('/grantPermission',function(req,res){
         });*/
 >>>>>>> e30c22b20fcc39f9701e29e70eb563a377d34a3b
 
-    /* for(var i=1;i<len;i++){
-        console.log(parseInt(req.query.ap1))
-        var bc=parseInt(req.query.ap1);
-        console.log("bc"+bc)
-         if(bc=="1"||bc=="2")
-         {
-         console.log("hey")
-        console.log(req.query.i)
-      con.query('update requests set pd=? where rid=?',[1,i],function(err,result){
-             console.log("hooo"+err)
-             console.log(result)
-             res.render("dashboardDean",{message:"Cool"});
-                   });
-                   }
-              }
-        });*/
+/* for(var i=1;i<len;i++){
+    console.log(parseInt(req.query.ap1))
+    var bc=parseInt(req.query.ap1);
+    console.log("bc"+bc)
+     if(bc=="1"||bc=="2")
+     {
+     console.log("hey")
+    console.log(req.query.i)
+  con.query('update requests set pd=? where rid=?',[1,i],function(err,result){
+         console.log("hooo"+err)
+         console.log(result)
+         res.render("dashboardDean",{message:"Cool"});
+               });
+               }
+          }
+    });*/
 
 
-      
-             
-         
-        
-   
 
 
-        
-           app.get('/SudhamaniHallInfo',function(req,res){
-            var request = "Select * from requests where Hname='Sudhamani';"
-        
-            con.query(request,function(req,data){
-                console.log(data);
-                res.render('SudhamaniHallInfo',{res:data});
-            });
-             })
-
-        app.get('/ValmikiHallInfo',function(req,res){
-            var request = "Select * from requests where Hname='Valmiki';"
-        
-            con.query(request,function(req,data){
-                console.log(data);
-                res.render('ValmikiHallInfo',{res:data});
-            });
-        })
 
 
-        app.get('/VyasaHallInfo',function(req,res){
-            var request = "Select * from requests where Hname='Vyasa';"
-        
-            con.query(request,function(req,data){
-                console.log(data);
-                res.render('VyasaHallInfo',{res:data});
-            });
-        })
 
 
-        app.get('/RamaHallInfo',function(req,res){
-            var request = "Select * from requests where Hname='Rama';"
-        
-            con.query(request,function(req,data){
-                console.log(data);
-                res.render('RamaHallInfo',{res:data});
-            });
-        })
 
 
-        app.get('/KrishnaHallInfo',function(req,res){
-            var request = "Select * from requests where Hname='Krishna';"
-        
-            con.query(request,function(req,data){
-                console.log(data);
-                res.render('KrishnaHallInfo',{res:data});
-            });
-        })
+app.get('/SudhamaniHallInfo', function (req, res) {
+    var request = "Select * from requests where Hname='Sudhamani';"
+
+    con.query(request, function (req, data) {
+        console.log(data);
+        res.render('SudhamaniHallInfo', { res: data });
+    });
+})
+
+app.get('/ValmikiHallInfo', function (req, res) {
+    var request = "Select * from requests where Hname='Valmiki';"
+
+    con.query(request, function (req, data) {
+        console.log(data);
+        res.render('ValmikiHallInfo', { res: data });
+    });
+})
 
 
-        app.get('/E-LearningHallInfo',function(req,res){
-            var request = "Select * from requests where Hname='ELearning';"
-        
-            con.query(request,function(req,data){
-                console.log(data);
-                res.render('E-LearningHallInfo',{res:data});
-            });
-        })
+app.get('/VyasaHallInfo', function (req, res) {
+    var request = "Select * from requests where Hname='Vyasa';"
+
+    con.query(request, function (req, data) {
+        console.log(data);
+        res.render('VyasaHallInfo', { res: data });
+    });
+})
 
 
-        app.get('/AmriteshwariHallInfo',function(req,res){
-            var request = "Select * from requests where Hname='Amriteshwari';"
-        
-            con.query(request,function(req,data){
-                console.log(data);
-                res.render('AmriteshwariHallInfo',{res:data});
-            });
-        })
+app.get('/RamaHallInfo', function (req, res) {
+    var request = "Select * from requests where Hname='Rama';"
+
+    con.query(request, function (req, data) {
+        console.log(data);
+        res.render('RamaHallInfo', { res: data });
+    });
+})
 
 
-     
+app.get('/KrishnaHallInfo', function (req, res) {
+    var request = "Select * from requests where Hname='Krishna';"
 
-app.get('/grantPermission',function(req,res){
-    var len= req.query.len;
-    console.log("the length"+len);
-     var stat= req.query.status2;
-    
+    con.query(request, function (req, data) {
+        console.log(data);
+        res.render('KrishnaHallInfo', { res: data });
+    });
+})
+
+
+app.get('/E-LearningHallInfo', function (req, res) {
+    var request = "Select * from requests where Hname='ELearning';"
+
+    con.query(request, function (req, data) {
+        console.log(data);
+        res.render('E-LearningHallInfo', { res: data });
+    });
+})
+
+
+app.get('/AmriteshwariHallInfo', function (req, res) {
+    var request = "Select * from requests where Hname='Amriteshwari';"
+
+    con.query(request, function (req, data) {
+        console.log(data);
+        res.render('AmriteshwariHallInfo', { res: data });
+    });
+})
+
+
+
+
+app.get('/grantPermission', function (req, res) {
+    var len = req.query.len;
+    console.log("the length" + len);
+    var stat = req.query.status2;
+
     console.log(stat);
-   for (var i=1;i<=len;i++){}
-     var pd = req.body.status+i;
-   console.log("ans"+status+i);
-    con.query('insert into requests set ?',pd,function(err,result){
-        if(!err){console.log("success");
-        res.render("/pdean",{message:"Yaay! It Worked!!!"});
-        }else{
+    for (var i = 1; i <= len; i++) { }
+    var pd = req.body.status + i;
+    console.log("ans" + status + i);
+    con.query('insert into requests set ?', pd, function (err, result) {
+        if (!err) {
+            console.log("success");
+            res.render("/pdean", { message: "Yaay! It Worked!!!" });
+        } else {
             console.log(err);
-            res.render("error",{message:"not working."});
-      }
-   });
+            res.render("error", { message: "not working." });
+        }
+    });
 });
 
 
